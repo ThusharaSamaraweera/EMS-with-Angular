5c25d277faad07bfaf1ac7b6b28aaa5056efe725
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-10-14 19:44:05 +0530

    add Table and filte in employee template

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import employees from './data/employees.json';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
+  title: string = 'Employee Management System';
+  employees: any[] = employees;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
