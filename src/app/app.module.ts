5934b528b6ff5318035d26829bcc609a1a2ca637
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-10-17 10:17:46 +0530

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { LkrFormatterPipe } from './shared/lkr-formatter.pipe';
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
-import { LkrFormatterPipe } from './shared/lkr-formatter.pipe';
 import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     LkrFormatterPipe,
-    ProgressBarComponent
+    ProgressBarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'employees',
+        component: EmployeesComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      }
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
